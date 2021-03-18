632d5a685ae12ea0841ae04ebcf02533c2a5d9b5
farinap5 <farinap5@protonmail.com>
2021-03-18 11:47:31 -0300

    New samples

---
+++ bool.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+// Main function
+int main(void) {
+    
+}
---
+++ condition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Main function 
+int main(void) {
+    // Declare integer variable
+    int num;
+
+    // Assigning value to variable
+    num = 5;
+    // Establishing a condition
+    // and comparing values
+    if (num > 10) {
+        printf(num," is greater than 10\n");
+    } else {
+        printf(num," is not greater than 10\n");
+    }
+    return 0;
+}
